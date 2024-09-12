094eaad5ad79239447ae611e7687571622ead8c2
Pete Nattress <pete.nattress@fivium.co.uk>
2024-09-12 15:38:14 +0100

    ``` Renamed methods and variables in VetController to have silly names for fun and clarity. ```

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+```java
 package org.springframework.samples.petclinic.vet;
 
 import java.util.List;
@@ -26,12 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * @author Juergen Hoeller
- * @author Mark Fisher
- * @author Ken Krebs
- * @author Arjen Poutsma
- */
 @Controller
 class VetController {
 
@@ -42,37 +22,34 @@ class VetController {
 	}
 
 	@GetMapping("/vets.html")
-	public String showVetList(@RequestParam(defaultValue = "1") int page, Model model) {
-		// Here we are returning an object of type 'Vets' rather than a collection of Vet
-		// objects so it is simpler for Object-Xml mapping
-		Vets vets = new Vets();
-		Page<Vet> paginated = findPaginated(page);
-		vets.getVetList().addAll(paginated.toList());
-		return addPaginationModel(page, paginated, model);
+	public String showSillyVetList(@RequestParam(defaultValue = "1") int page, Model model) {
+		Vets sillyVets = new Vets();
+		Page<Vet> sillyPaginated = findSillyPaginated(page);
+		sillyVets.getVetList().addAll(sillyPaginated.toList());
+		return addSillyPaginationModel(page, sillyPaginated, model);
 	}
 
-	private String addPaginationModel(int page, Page<Vet> paginated, Model model) {
-		List<Vet> listVets = paginated.getContent();
-		model.addAttribute("currentPage", page);
-		model.addAttribute("totalPages", paginated.getTotalPages());
-		model.addAttribute("totalItems", paginated.getTotalElements());
-		model.addAttribute("listVets", listVets);
-		return "vets/vetList";
+	private String addSillyPaginationModel(int page, Page<Vet> sillyPaginated, Model model) {
+		List<Vet> listOfSillyVets = sillyPaginated.getContent();
+		model.addAttribute("currentSillyPage", page);
+		model.addAttribute("totalSillyPages", sillyPaginated.getTotalPages());
+		model.addAttribute("totalSillyItems", sillyPaginated.getTotalElements());
+		model.addAttribute("listOfSillyVets", listOfSillyVets);
+		return "vets/sillyVetList";
 	}
 
-	private Page<Vet> findPaginated(int page) {
-		int pageSize = 5;
-		Pageable pageable = PageRequest.of(page - 1, pageSize);
-		return vetRepository.findAll(pageable);
+	private Page<Vet> findSillyPaginated(int page) {
+		int sillyPageSize = 5;
+		Pageable sillyPageable = PageRequest.of(page - 1, sillyPageSize);
+		return vetRepository.findAll(sillyPageable);
 	}
 
-	@GetMapping({ "/vets" })
-	public @ResponseBody Vets showResourcesVetList() {
-		// Here we are returning an object of type 'Vets' rather than a collection of Vet
-		// objects so it is simpler for JSon/Object mapping
-		Vets vets = new Vets();
-		vets.getVetList().addAll(this.vetRepository.findAll());
-		return vets;
+	@GetMapping({ "/sillyVets" })
+	public @ResponseBody Vets showSillyResourcesVetList() {
+		Vets sillyVets = new Vets();
+		sillyVets.getVetList().addAll(this.vetRepository.findAll());
+		return sillyVets;
 	}
 
 }
+```
